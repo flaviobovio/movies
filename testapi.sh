1dc52b09515256a7ee3bbebe581de0cd7f872c16
ozzy <flavioboviovt@gmail.com>
2023-08-10 18:25:44 -0300

    final preview 3

---
+++ testapi.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+echo "\n=== Test peliculas_idioma ==="
+export rq=https://my-python-app-ei4zqecxya-uc.a.run.app/peliculas_idioma/en
+echo "request"
+echo $rq
+echo "response"
+curl $rq
+
+
+echo "\n\n=== Test peliculas_duracion ==="
+export rq=https://my-python-app-ei4zqecxya-uc.a.run.app/peliculas_duracion/the%20terminator
+echo "request"
+echo $rq
+echo "response"
+curl $rq
+
+echo "\n\n=== Test franquicia ==="
+export rq=https://my-python-app-ei4zqecxya-uc.a.run.app/franquicia/toy%20story%20collection
+echo "request"
+echo $rq
+echo "response"
+curl $rq
+
+echo "\n\n=== Test peliculas_pais ==="
+export rq=https://my-python-app-ei4zqecxya-uc.a.run.app/peliculas_pais/United%20States%20of%20America
+echo "request"
+echo $rq
+echo "response"
+curl $rq
+
+
+
+
+
+
+echo "\n\n=== Test productoras_exitosas ==="
+export rq=https://my-python-app-ei4zqecxya-uc.a.run.app/productoras_exitosas/TriStar
+echo "request"
+echo $rq
+echo "response"
+curl $rq
+
+
+echo "\n\n=== Test get_director ==="
+export rq=https://my-python-app-ei4zqecxya-uc.a.run.app/get_director/James%20Gunn
+echo "request"
+echo $rq
+echo "response"
+curl $rq
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo "\n\n=== Test recomendations ==="
+export rq="https://my-python-app-ei4zqecxya-uc.a.run.app/recomendacion/cars"
+echo "request"
+echo $rq
+echo "response"
+curl $rq
+echo "\n"
+
+
+
